dee5c7f873168095986b843ba5c538a5bed9fc8f
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-01 21:25:03 +0530

    Added to be changed

--- Automation.java
+++ Automation.java
@@ -3,5 +3,6 @@ package Amazon;
 public class Automation {
     public static void main(String[] args) {
         System.out.println("This is Automation");
+        System.out.println("Automation tools are: Selelium, Jeera,   ");
     }
 }
